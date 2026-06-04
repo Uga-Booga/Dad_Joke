a9cc461974c57b1d63062ebdb64237d51b539bcd
Uga-Booga <seixasben@gmail.com>
2026-06-03 22:56:34 -0300

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Just a dad joke generator.
+
+Can be installed on Linux with .deb or .rpm.(Working on relesing to Windows and maybe MacOS)
+
+Powered by icanhazdadjoke.com API
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    system("curl -H \"Accept: text/plain\" https://icanhazdadjoke.com/");
+    return 0;
+}
